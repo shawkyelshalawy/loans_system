f37db6528bf189380a0565e9d7927d32f64e6202
shawkyelshalawy <shawkyfariedelshalawy@gmail.com>
2025-03-09 22:09:13 +0200

    wrapping up the app

--- bank_system/urls.py
+++ bank_system/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('loans.urls')),  # Include our app's API endpoints
+    path('api/', include('loans.urls')), 
 ]
